c3b54d29e9fb77f32325137784857e54f07d9788
Donald Uzochukwu <121743831+Donducci@users.noreply.github.com>
2023-07-31 22:22:42 +0000

    user creadted in the backend

---
+++ src/front/js/component/login.js
@@ -0,0 +1,68 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
+
+export const LogIn = ({ onClose }) => {
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    if (email === "" || password === "") {
+      alert("The field cannot be empty.");
+      return;
+    }
+
+    try {
+      const response = await actions.login(email, password);
+      console.log(response);
+
+      if (response.token) {
+        setTimeout(() => {
+          onClose();
+          navigate("/");
+        }, 2000);
+      } else {
+        alert("Wrong email or password.");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Login failed!");
+    }
+  };
+
+  return (
+    <form className="signup-form-wrapper" onSubmit={handleLogin}>
+      <div className="form-group">
+        <label>Email</label>
+        <input
+          className="login-form-input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+        />
+      </div>
+      <div className="form-group">
+        <label>Password</label>
+        <input
+          className="signup-form-input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+        />
+      </div>
+      <button className="navbar-button" type="submit">
+        Log In
+      </button>
+    </form>
+  );
+};
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,38 +1,29 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			token: "",
+			user: {},
+			apiUrl: process.env.BACKEND_URL,
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
+			// Use getActions to call a function within a function
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
 
 			getMessage: async () => {
-				try{
+				try {
 					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
+					const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+					const data = await resp.json();
+					setStore({ message: data.message });
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+				} catch (error) {
+					console.log("Error loading message from backend", error);
 				}
 			},
+
 			changeColor: (index, color) => {
 				//get the store
 				const store = getStore();
@@ -46,8 +37,99 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
-			}
-		}
+			},
+
+			// signUp: async (email, password) => {
+			// 	try {
+			// 		const response = await fetch(process.env.BACKEND_URL + "/api/sign-up", {
+			// 			method: "POST",
+			// 			headers: {
+			// 				"Content-Type": "application/json",
+			// 			},
+			// 			body: JSON.stringify({
+			// 				email,
+			// 				password,
+			// 			}),
+			// 		});
+
+			// 		if (response.ok) {
+			// 			const data = await response.json();
+			// 			console.log("User Created:", data)
+			// 		} else {
+			// 			console.log("Failed to create user.");
+			// 			const errorData = await response.json();
+			// 			console.log("Error:", errorData);
+			// 		}
+			// 	} catch (error) {
+			// 		console.log("An error occurred:", error);
+			// 	}
+			// },
+
+			login: async (email, password) => {
+				try {
+					const response = await fetch(process.env.BACKEND_URL + "/api/log-in", {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({
+							email: email,
+							password: password,
+						}),
+					});
+
+					if (response.ok) {
+						const data = await response.json();
+						setStore({ user: data });
+						localStorage.setItem("user", JSON.stringify(data));
+						return data;
+					} else {
+						throw new Error("Login failed");
+					}
+				} catch (error) {
+					throw new Error("Login failed");
+				}
+			},
+
+
+
+
+
+			verifyUser: (token) => {
+				fetch(process.env.BACKEND_URL + "/api/user-by-token", {
+					method: "GET",
+					headers: {
+						Authorization: "Bearer " + token
+					},
+					redirect: "follow",
+				})
+					.then((response) => response.json())
+					.then((result) => setStore({ user: result }))
+					.catch((error) => console.log(error))
+
+			},
+
+
+			checkUser: () => {
+				if (localStorage.getItem("token")) {
+					setStore({
+						token: JSON.parse(localStorage.getItem("token")),
+						user: JSON.parse(localStorage.getItem("user")),
+					});
+				}
+			},
+
+			logout: () => {
+				setStore({
+					token: "",
+					user: {},
+					posts: [],
+					userPosts: [],
+				});
+				localStorage.removeItem("token");
+				localStorage.removeItem("user");
+			},
+		},
 	};
 };
 
